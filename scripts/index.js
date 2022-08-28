113248e8cfb5f8ef52e1ed9e43e710d48d0b8e2d
Iris Rademacher <84595986+iris-garden@users.noreply.github.com>
2022-08-28 02:06:06 -0400

    zone

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,15 @@
-const audioElement = new Audio('media/educon.mp3')
-audioElement.play()
-let background = [128, 128, 128]
-let delta = 100
-setInterval(() => {
-  background = background.map((bit) => Math.min(Math.max(bit + ((Math.random() < 0.5 ? -1 : 1) * Math.floor(Math.random() * delta)), 0), 255))
-  const [r, g, b] = background
-  document.getElementById('bkgd').style.backgroundColor = `rgb(${r},${g},${b})`
-}, 100)
+const main = () => {
+  const audioElement = new Audio('media/educon.mp3')
+  audioElement.play()
+  let background = [128, 128, 128]
+  let delta = 100
+  setInterval(() => {
+    background = background.map((bit) => Math.min(Math.max(bit + ((Math.random() < 0.5 ? -1 : 1) * Math.floor(Math.random() * delta)), 0), 255))
+    const [r, g, b] = background
+    document.getElementById('bkgd').style.backgroundColor = `rgb(${r},${g},${b})`
+  }, 100)
+  const mainEle = document.getElementById("main")
+  mainEle.className = "card"
+  mainEle.removeEventListener('click', main)
+}
+document.getElementById("main").addEventListener('click', main)
